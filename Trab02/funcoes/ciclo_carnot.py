fd5eff6adc9d3162d148226b98caddc38dd6c207
Andre <andrefloriano27@hotmail.comm>
2025-11-17 19:01:59 -0300

    Adição pressões e correção

--- Trab02/funcoes/ciclo_carnot.py
+++ Trab02/funcoes/ciclo_carnot.py
@@ -14,10 +14,14 @@ def carnot_temps(Th, Tl, Ql, liq_ref):
     carnot_temps_dict={
         "COP": COP,
         "w": Ql/COP,
+        "H1": CP.PropsSI("H", "T", Tl, "S", S2, liq_ref),
         "H2": CP.PropsSI("H", "T", Th, "Q", 1, liq_ref),
-        "H1": CP.PropsSI("H", "T", Th, "S", S2, liq_ref),
         "H3": CP.PropsSI("H", "T", Th, "Q", 0, liq_ref),
-        "H4": CP.PropsSI("H", "T", Th, "S", S3, liq_ref),
+        "H4": CP.PropsSI("H", "T", Tl, "S", S3, liq_ref),
+        "P1": CP.PropsSI("P", "T", Tl, "S", S2, liq_ref),
+        "P2": CP.PropsSI("P", "T", Th, "Q", 1, liq_ref),
+        "P3": CP.PropsSI("P", "T", Th, "Q", 0, liq_ref),
+        "P4": CP.PropsSI("P", "T", Tl, "S", S3, liq_ref),
     }
 
     return carnot_temps_dict
--- Trab02/main.py
+++ Trab02/main.py
@@ -10,6 +10,7 @@ Capacidade_necessaria = 15000 * BTU_to_W
 T_ext_calor = 35 + C_to_K # Temperatura externa
 T_interior = 18 + C_to_K
 
+#* Condições ideais:
 carnot_temps_dict = carnot_temps(Th=T_ext_calor, Tl=T_interior, Ql=Capacidade_necessaria, liq_ref=liq_ref)
 
-print(carnot_temps_dict)
+print("\nCondições Ideais:\n", carnot_temps_dict)
