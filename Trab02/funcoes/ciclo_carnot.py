6476568f6b28773bf1859365f22b8a233381c690
Andre <andrefloriano27@hotmail.comm>
2025-11-17 18:55:44 -0300

    carnot temps validado

---
+++ Trab02/funcoes/ciclo_carnot.py
@@ -0,0 +1,23 @@
+import CoolProp.CoolProp as CP
+import pandas as pd
+import numpy as np
+
+
+
+
+def carnot_temps(Th, Tl, Ql, liq_ref):
+    COP = Tl/(Th-Tl)
+
+    S2 = CP.PropsSI("S", "T", Th, "Q", 1, liq_ref)
+    S3 = CP.PropsSI("S", "T", Th, "Q", 0, liq_ref)
+
+    carnot_temps_dict={
+        "COP": COP,
+        "w": Ql/COP,
+        "H2": CP.PropsSI("H", "T", Th, "Q", 1, liq_ref),
+        "H1": CP.PropsSI("H", "T", Th, "S", S2, liq_ref),
+        "H3": CP.PropsSI("H", "T", Th, "Q", 0, liq_ref),
+        "H4": CP.PropsSI("H", "T", Th, "S", S3, liq_ref),
+    }
+
+    return carnot_temps_dict
---
+++ Trab02/main.py
@@ -0,0 +1,15 @@
+from funcoes.ciclo_carnot import *
+
+#* Conversões:
+BTU_to_W = 0.293
+C_to_K = 273
+
+#* Condições de contorno:
+liq_ref = "R134a"
+Capacidade_necessaria = 15000 * BTU_to_W
+T_ext_calor = 35 + C_to_K # Temperatura externa
+T_interior = 18 + C_to_K
+
+carnot_temps_dict = carnot_temps(Th=T_ext_calor, Tl=T_interior, Ql=Capacidade_necessaria, liq_ref=liq_ref)
+
+print(carnot_temps_dict)
