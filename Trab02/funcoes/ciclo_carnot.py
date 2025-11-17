fd75dff1606e2febe7bf8f03c2308ea9d8108e3d
Andre <andrefloriano27@hotmail.comm>
2025-11-17 19:15:20 -0300

    Cálculo Bomba de calor

--- Trab02/funcoes/ciclo_carnot.py
+++ Trab02/funcoes/ciclo_carnot.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 
-def carnot_temps(Th, Tl, Ql, liq_ref):
+def carnot_temps_verao(Th, Tl, QL, liq_ref):
     COP = Tl/(Th-Tl)
 
     S2 = CP.PropsSI("S", "T", Th, "Q", 1, liq_ref)
@@ -13,7 +13,32 @@ def carnot_temps(Th, Tl, Ql, liq_ref):
 
     carnot_temps_dict={
         "COP": COP,
-        "w": Ql/COP,
+        "w": QL/COP,
+        "H1": CP.PropsSI("H", "T", Tl, "S", S2, liq_ref),
+        "H2": CP.PropsSI("H", "T", Th, "Q", 1, liq_ref),
+        "H3": CP.PropsSI("H", "T", Th, "Q", 0, liq_ref),
+        "H4": CP.PropsSI("H", "T", Tl, "S", S3, liq_ref),
+        "P1": CP.PropsSI("P", "T", Tl, "S", S2, liq_ref),
+        "P2": CP.PropsSI("P", "T", Th, "Q", 1, liq_ref),
+        "P3": CP.PropsSI("P", "T", Th, "Q", 0, liq_ref),
+        "P4": CP.PropsSI("P", "T", Tl, "S", S3, liq_ref),
+    }
+
+    return carnot_temps_dict
+
+def carnot_temps_inverno(Th, Tl, QH, liq_ref):
+    COP = Tl/(Th-Tl)
+    QL= COP*QH/(COP+1)
+    w = QL/COP
+
+
+    S2 = CP.PropsSI("S", "T", Th, "Q", 1, liq_ref)
+    S3 = CP.PropsSI("S", "T", Th, "Q", 0, liq_ref)
+
+    carnot_temps_dict={
+        "COP": COP,
+        "QL": QL,
+        "w": QL/COP,
         "H1": CP.PropsSI("H", "T", Tl, "S", S2, liq_ref),
         "H2": CP.PropsSI("H", "T", Th, "Q", 1, liq_ref),
         "H3": CP.PropsSI("H", "T", Th, "Q", 0, liq_ref),
--- Trab02/main.py
+++ Trab02/main.py
@@ -7,10 +7,13 @@ C_to_K = 273
 #* Condições de contorno:
 liq_ref = "R134a"
 Capacidade_necessaria = 15000 * BTU_to_W
-T_ext_calor = 35 + C_to_K # Temperatura externa
+T_ext_verao = 35 + C_to_K # Temperatura externa no verao
+T_ext_inverno = 9 + C_to_K # Temperatura externa no inverno
 T_interior = 18 + C_to_K
 
 #* Condições ideais:
-carnot_temps_dict = carnot_temps(Th=T_ext_calor, Tl=T_interior, Ql=Capacidade_necessaria, liq_ref=liq_ref)
+carnot_verao = carnot_temps_verao(Th=T_ext_verao, Tl=T_interior, QL=Capacidade_necessaria, liq_ref=liq_ref)
+carnot_inverno = carnot_temps_inverno(Th=T_interior, Tl=T_ext_inverno, QH=Capacidade_necessaria, liq_ref=liq_ref)
 
-print("\nCondições Ideais:\n", carnot_temps_dict)
+print("\nCondições Ideais Verão:\n", carnot_verao)
+print("\nCondições Ideais Inverno:\n", carnot_inverno)
